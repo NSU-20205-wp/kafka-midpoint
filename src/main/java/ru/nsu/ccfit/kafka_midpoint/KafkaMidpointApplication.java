8020a6096c5bcf16fddfa0c067e5359f5e408349
AleksandrMatsko <a.matsko@g.nsu.ru>
2023-04-07 15:38:59 +0700

    created empty spring boot project

---
+++ src/main/java/ru/nsu/ccfit/kafka_midpoint/KafkaMidpointApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.kafka_midpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMidpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMidpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nsu/ccfit/kafka_midpoint/KafkaMidpointApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.kafka_midpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMidpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
