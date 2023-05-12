2d5ed1c47dae95955d756910931606a4fae47ec1
AleksandrMatsko <a.matsko@g.nsu.ru>
2023-05-12 16:44:58 +0700

    fix: structure application.properties, change usage of topic and groupId for KafkaConsumer

--- src/main/java/ru/nsu/ccfit/kafka_midpoint/kafka/consumer/MyKafkaListener.java
+++ src/main/java/ru/nsu/ccfit/kafka_midpoint/kafka/consumer/MyKafkaListener.java
@@ -7,13 +7,13 @@ import org.springframework.stereotype.Component;
 import java.util.logging.Logger;
 
 @Component
-@KafkaListener(id = "multiId", topics = "multiTopic")
+@KafkaListener(topics = "${topic.name.consumer}", groupId = "${spring.kafka.consumer.group-id}")
 public class MyKafkaListener {
     private static final Logger logger = Logger.getLogger(MyKafkaListener.class.getName());
 
     @KafkaHandler(isDefault = true)
-    public void messageHandler(Object object) {
-        logger.info(() -> "\nReceived: " + object + " object type: " + object.getClass() + "\n");
+    public void messageHandler(String msg) {
+        logger.info(() -> "Received: " + msg + "\n");
     }
 
 }
