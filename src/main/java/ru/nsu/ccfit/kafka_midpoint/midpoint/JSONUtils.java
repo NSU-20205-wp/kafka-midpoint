8d61344b083b44d9b7353717efc91e4c785c4740
NDymont <91301384+NDymont@users.noreply.github.com>
2023-05-19 22:06:17 +0700

    feat: added new method in JsonUtils

--- src/main/java/ru/nsu/ccfit/kafka_midpoint/midpoint/JSONUtils.java
+++ src/main/java/ru/nsu/ccfit/kafka_midpoint/midpoint/JSONUtils.java
@@ -1,12 +1,23 @@
 package ru.nsu.ccfit.kafka_midpoint.midpoint;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.json.JSONObject;
 
 public class JSONUtils {
-    public static JSONObject wrapper(JSONObject roleJson, String objectType) throws JsonProcessingException {
+    public static JSONObject wrapper(JSONObject roleJson, String objectType) {
         JSONObject root = new JSONObject();
         root.put(objectType, roleJson);
         return root;
+    }
+
+    public static String wrapper(String key, String value) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode rootNode = mapper.createObjectNode();
+        rootNode.set(key, mapper.readTree(value));
+        System.out.println(mapper.writeValueAsString(rootNode));
+        return mapper.writeValueAsString(rootNode);
+
     }
 }
