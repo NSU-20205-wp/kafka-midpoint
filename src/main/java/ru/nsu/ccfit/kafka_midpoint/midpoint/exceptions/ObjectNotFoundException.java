99510289adfd0c7ea9a730f6381aadc441401f98
NDymont <91301384+NDymont@users.noreply.github.com>
2023-05-19 16:46:49 +0700

    feat: added MidpointExceptions

---
+++ src/main/java/ru/nsu/ccfit/kafka_midpoint/midpoint/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.nsu.ccfit.kafka_midpoint.midpoint.exceptions;
+
+public class ObjectNotFoundException extends MidpointException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
