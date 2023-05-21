bfff2c37d24d7e0502665d4431acdcb540212d81
NDymont <91301384+NDymont@users.noreply.github.com>
2023-05-22 01:15:58 +0700

    feat: added more options in modifier

--- src/main/java/ru/nsu/ccfit/kafka_midpoint/midpoint/dtos/ConnectorDTO.java
+++ src/main/java/ru/nsu/ccfit/kafka_midpoint/midpoint/dtos/ConnectorDTO.java
@@ -1,11 +1,13 @@
 package ru.nsu.ccfit.kafka_midpoint.midpoint.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ConnectorDTO extends MidpointDTO{
 
     private String displayName;
     private String description;
-
-    // Конструкторы, геттеры и сеттеры
 
 }
