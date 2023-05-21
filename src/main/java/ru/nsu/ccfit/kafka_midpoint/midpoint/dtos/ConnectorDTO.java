66c134a8c9619f95c45302e8af76d8cede6a5486
NDymont <91301384+NDymont@users.noreply.github.com>
2023-05-21 23:44:19 +0700

    feat: added DTO for resources

---
+++ src/main/java/ru/nsu/ccfit/kafka_midpoint/midpoint/dtos/ConnectorDTO.java
@@ -0,0 +1,11 @@
+package ru.nsu.ccfit.kafka_midpoint.midpoint.dtos;
+
+
+public class ConnectorDTO extends MidpointDTO{
+
+    private String displayName;
+    private String description;
+
+    // Конструкторы, геттеры и сеттеры
+
+}
