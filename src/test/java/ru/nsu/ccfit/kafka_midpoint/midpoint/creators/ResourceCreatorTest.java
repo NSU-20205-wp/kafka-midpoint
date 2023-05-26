e15442715d046bc5cf0f15c6ab200449fa1b8a1a
mentalMint <walkershush@gmail.com>
2023-05-27 00:08:36 +0700

    feat: add tests for exceptional situations

--- src/test/java/ru/nsu/ccfit/kafka_midpoint/midpoint/creators/ResourceCreatorTest.java
+++ src/test/java/ru/nsu/ccfit/kafka_midpoint/midpoint/creators/ResourceCreatorTest.java
@@ -5,12 +5,9 @@ import ru.nsu.ccfit.kafka_midpoint.midpoint.MidpointCreator;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.MidpointDeleter;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.MidpointSearcher;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.deleters.ResourceDeleter;
-import ru.nsu.ccfit.kafka_midpoint.midpoint.deleters.RoleDeleter;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.dtos.ResourceDTO;
-import ru.nsu.ccfit.kafka_midpoint.midpoint.dtos.RoleDTO;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.factory.creator.ProductCreatorException;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.searchers.ResourceSearcher;
-import ru.nsu.ccfit.kafka_midpoint.midpoint.searchers.RoleSearcher;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,12 +15,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static ru.nsu.ccfit.kafka_midpoint.utilities.StringUtilities.generateString;
 
 class ResourceCreatorTest {
-
     @Test
-    void testSendRequest() throws IOException, ProductCreatorException {
+    void testNormalCreateRequest() throws IOException, ProductCreatorException {
         String name = generateString(10);
         MidpointCreator creator = new ResourceCreator();
         Map<String, Object> createParams = new HashMap<>();
@@ -45,5 +42,63 @@ class ResourceCreatorTest {
         deleter.doOperation(createParams);
         responseCode = deleter.getResponseCode();
         assertEquals(2, responseCode / 100);
+    }
+
+    @Test
+    void testDoubleCreateRequest() throws IOException, ProductCreatorException {
+        String name = generateString(10);
+        MidpointCreator creator = new ResourceCreator();
+        Map<String, Object> createParams = new HashMap<>();
+        createParams.put("name", name);
+        creator.doOperation(createParams);
+        int responseCode = creator.getResponseCode();
+        assertEquals(2, responseCode / 100);
+
+        MidpointCreator creator2 = new ResourceCreator();
+        creator2.doOperation(createParams);
+        responseCode = creator2.getResponseCode();
+        assertEquals(4, responseCode / 100);
+
+        MidpointSearcher searcher = new ResourceSearcher();
+        Map<String, Object> searchParams = new HashMap<>();
+        searchParams.put("fieldName", "name");
+        searchParams.put("value", name);
+        ArrayList<ResourceDTO> response = (ArrayList<ResourceDTO>) searcher.doOperation(searchParams);
+        responseCode = searcher.getResponseCode();
+        assertEquals(2, responseCode / 100);
+        assertEquals(createParams.get("name"), response.get(0).getName());
+
+        MidpointDeleter deleter = new ResourceDeleter();
+        deleter.doOperation(createParams);
+        responseCode = deleter.getResponseCode();
+        assertEquals(2, responseCode / 100);
+    }
+
+    @Test
+    void testNormalCreateRequestWithEmptyParams() throws IOException, ProductCreatorException {
+        MidpointCreator creator = new ResourceCreator();
+        Map<String, Object> createParams = new HashMap<>();
+        creator.doOperation(createParams);
+        int responseCode = creator.getResponseCode();
+        assertEquals(5, responseCode / 100);
+    }
+
+    @Test
+    void testNormalCreateRequestWithNullParams() throws IOException {
+        MidpointCreator creator = new ResourceCreator();
+        assertThrows(NullPointerException.class, () -> {
+            creator.doOperation(null);
+        });
+    }
+
+    @Test
+    void testNormalCreateRequestWithWrongParams() throws IOException {
+        String name = generateString(10);
+        MidpointCreator creator = new ResourceCreator();
+        Map<String, Object> createParams = new HashMap<>();
+        createParams.put("", name);
+        assertThrows(ProductCreatorException.class, () -> {
+            creator.doOperation(createParams);
+        });
     }
 }
--- src/test/java/ru/nsu/ccfit/kafka_midpoint/midpoint/creators/UserCreatorTest.java
+++ src/test/java/ru/nsu/ccfit/kafka_midpoint/midpoint/creators/UserCreatorTest.java
@@ -1,30 +1,25 @@
 package ru.nsu.ccfit.kafka_midpoint.midpoint.creators;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.jupiter.api.Test;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.MidpointCreator;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.MidpointDeleter;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.MidpointSearcher;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.deleters.UserDeleter;
-import ru.nsu.ccfit.kafka_midpoint.midpoint.dtos.MidpointDTO;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.dtos.UserDTO;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.exceptions.ObjectNotFoundException;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.factory.creator.ProductCreatorException;
 import ru.nsu.ccfit.kafka_midpoint.midpoint.searchers.UserSearcher;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static ru.nsu.ccfit.kafka_midpoint.utilities.StringUtilities.generateString;
 
 class UserCreatorTest {
-
     @Test
     void testSendRequest() throws IOException, ObjectNotFoundException, ProductCreatorException {
         String name = generateString(10);
@@ -48,5 +43,63 @@ class UserCreatorTest {
         deleter.doOperation(createParams);
         responseCode = deleter.getResponseCode();
         assertEquals(2, responseCode / 100);
+    }
+
+    @Test
+    void testDoubleCreateRequest() throws IOException, ProductCreatorException {
+        String name = generateString(10);
+        MidpointCreator creator = new UserCreator();
+        Map<String, Object> createParams = new HashMap<>();
+        createParams.put("name", name);
+        creator.doOperation(createParams);
+        int responseCode = creator.getResponseCode();
+        assertEquals(2, responseCode / 100);
+
+        MidpointCreator creator2 = new UserCreator();
+        creator2.doOperation(createParams);
+        responseCode = creator2.getResponseCode();
+        assertEquals(4, responseCode / 100);
+
+        MidpointSearcher searcher = new UserSearcher();
+        Map<String, Object> searchParams = new HashMap<>();
+        searchParams.put("fieldName", "name");
+        searchParams.put("value", name);
+        ArrayList<UserDTO> response = (ArrayList<UserDTO>) searcher.doOperation(searchParams);
+        responseCode = searcher.getResponseCode();
+        assertEquals(2, responseCode / 100);
+        assertEquals(createParams.get("name"), response.get(0).getName());
+
+        MidpointDeleter deleter = new UserDeleter();
+        deleter.doOperation(createParams);
+        responseCode = deleter.getResponseCode();
+        assertEquals(2, responseCode / 100);
+    }
+
+    @Test
+    void testNormalCreateRequestWithEmptyParams() throws IOException, ProductCreatorException {
+        MidpointCreator creator = new UserCreator();
+        Map<String, Object> createParams = new HashMap<>();
+        creator.doOperation(createParams);
+        int responseCode = creator.getResponseCode();
+        assertEquals(5, responseCode / 100);
+    }
+
+    @Test
+    void testNormalCreateRequestWithNullParams() throws IOException {
+        MidpointCreator creator = new UserCreator();
+        assertThrows(NullPointerException.class, () -> {
+            creator.doOperation(null);
+        });
+    }
+
+    @Test
+    void testNormalCreateRequestWithWrongParams() throws IOException {
+        String name = generateString(10);
+        MidpointCreator creator = new UserCreator();
+        Map<String, Object> createParams = new HashMap<>();
+        createParams.put("", name);
+        assertThrows(ProductCreatorException.class, () -> {
+            creator.doOperation(createParams);
+        });
     }
 }
